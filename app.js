1e0d12249bdeed1eaf1735e7a4cb0f79ec98a1d5
nggerber <gerberclimbs@gmail.com>
2020-12-06 13:37:44 -0700

    added inquirer

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const mysql = require("mysql")
 const inquirer = require("inquirer")
 
 var connection = mysql.createConnection({
+host: "localhost",
 
 port: 3306,
 
@@ -17,7 +18,28 @@ database: "employeeDB"
 connection.connect(function(err) {
 if (err) throw err;
 console.log("connected as id " + connection.threadId)
+userPrompt();
+});
+
+function userPrompt() {
+
+    console.log("prompt working")
+
+    inquirer.prompt ({
+        type: "list",
+        name: "userChoice",
+        message: "What would you like to do?",
+        choices: [
+            "View Employees",
+            "View Departments",
+            "View Roles",
+            "Add Employees",
+            " Updater your Employees",
+            "Add a new Role",
+            "Leave"
+        ]
+    })
 
 
 
-})
+}
