274a2d046ca91ac13ab849de5c7509a084e4faa5
nggerber <gerberclimbs@gmail.com>
2020-12-10 12:32:52 -0700

    fully functioning application

---
+++ README.md
@@ -0,0 +1,14 @@
+###### Employee Database
+
+This application was developed to give employers a quick and concise way to manage employees through the command line.
+
+
+!
+
+## Useage
+
+In order to use this application the user has to run npm install inquirer and the open the command terminal for the code. By then running node index.js in their terminal they will be asked a series of questions and after answering them a README.md will be generated in the folder.
+
+## Link to my GitHub
+
+https://github.com/Nggerber/readme-generator
--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const mysql = require("mysql")
 const inquirer = require("inquirer")
 
-
+// establish mysql connection
 var connection = mysql.createConnection({
     host: "localhost",
 
@@ -22,6 +22,7 @@ connection.connect(function (err) {
     userPrompt();
 });
 
+// function that uses inquirer to ask the user what they want to do
 function userPrompt() {
 
     console.log("prompt working")
@@ -36,10 +37,11 @@ function userPrompt() {
             "View Roles",
             "Add Employees",
             "Update your Employees",
-            "Add a New Role",
+            "Add a New Department",
             "Leave",
         ],
 
+        // pass the user choice into an if else to decide what path they enter    
     }).then(function ({ userChoice }) {
 
         if (userChoice === "View Employees") {
@@ -52,13 +54,13 @@ function userPrompt() {
             viewRoles();
         }
         else if (userChoice === "Add Employees") {
-
+            addEmployee();
         }
         else if (userChoice === "Update your Employees") {
-
+            updateEmployees();
         }
-        else if (userChoice === "Add a New Role") {
-
+        else if (userChoice === "Add a New Department") {
+            addDepartment();
         }
         else {
             connection.end();
@@ -67,6 +69,7 @@ function userPrompt() {
     })
 }
 
+//function that calls all DB data in a table form
 function viewEmployees() {
 
     connection.query("SELECT e.id, e.first_name, r.title, d.dep_name, r.salary, CONCAT (m.first_name, m.last_name) AS manager FROM employee e LEFT Join role r ON e.role_id = r.id LEFT JOIN department d ON d.id = r.department_id LEFT JOIN employee m ON m.id = e.manager_id", function (err, res) {
@@ -77,6 +80,7 @@ function viewEmployees() {
     })
 }
 
+//function that just calls department table
 function viewDepartments() {
 
     connection.query("SELECT * from department", function (err, res) {
@@ -87,6 +91,7 @@ function viewDepartments() {
     })
 }
 
+//function that call roles data
 function viewRoles() {
 
     connection.query("SELECT * from role", function (err, res) {
@@ -95,5 +100,107 @@ function viewRoles() {
         console.table(res);
         userPrompt();
     })
+}
+
+// function to add new employee to the database
+function addEmployee() {
+
+    // create object for the new employee
+    let newEmployee = [
+        {
+            type: "input",
+            message: "What is your new Employee's first name?",
+            name: "first_name",
+        },
+        {
+            type: "input",
+            message: "What is your new Employee's last name?",
+            name: "last_name",
+        },
+        {
+            type: "input",
+            message: "What is your new Employee's role ID?",
+            name: "role_id",
+        },
+        {
+            type: "input",
+            message: "What is your new Employee's manager's ID?",
+            name: "manager_id",
+        },
+    ];
+
+    //populate object with Inquirer questions
+    inquirer.prompt(newEmployee).then(function (answer) {
+        const query = `INSERT INTO employee SET ?`;
+
+        connection.query(
+            query,
+            {
+                first_name: answer.first_name,
+                last_name: answer.last_name,
+                role_id: answer.role_id,
+                manager_id: answer.manager_id,
+            },
+            function (err) {
+                if (err) throw err;
+
+                viewEmployees();
+                userPrompt();
+
+            }
+        )
+    })
+}
+
+function updateEmployees() {
+
+    inquirer.prompt([
+
+        {
+            type: "input",
+            name: "employeeName",
+            message: "Which employee would you like to update",
+        },
+        {
+            type: "list",
+            name: "newRole",
+            message: "What is the Id of your employees new role?",
+            choices: [
+                "1",
+                "2",
+                "3",
+                "4",
+                "5",
+            ]
+        }
+    ]).then(function (answer) {
+        connection.query(
+            "UPDATE employee SET role_id = ? Where first_name = ?",
+            [answer.newRole, answer.employeeName],
+            function (err, res) {
+                if (err) throw err;
+                viewEmployees();
+                userPrompt();
+            }
+        );
+    });
+}
+
+function addDepartment() {
+    inquirer.prompt ([
+        {
+            type: "input",
+            name: "newDepartment",
+            message: "What is the name of your new department?"
+
+        }
+    ]).then(function (answer) {
+        connection.query(
+            "INSERT INTO department (dep_name) value (?)",
+            [answer.newDepartment]
+        )
+        viewDepartments();
+    })
+
 }
 
